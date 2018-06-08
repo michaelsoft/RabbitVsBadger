daee1b7f476941a3eebba5a0e7ee11cfc75e6468
michaelsoft <michaellyk@qq.com>
2018-06-08 14:26:12 +0800

    ddd

--- game.py
+++ game.py
@@ -1,9 +1,13 @@
+# 1 - Import library
 import pygame
 from pygame.locals import *
-
+ 
+# 2 - Initialize the game
 pygame.init()
-width, height = 640,480
-screen = pygame.display.set_mode((width,height))
+width, height = 640, 480
+screen=pygame.display.set_mode((width, height))
+ 
+# 3 - Load images
 player = pygame.image.load("resources/images/dude.png")
 
 grass = pygame.image.load("resources/images/grass.png")
@@ -16,12 +20,19 @@ screen.blit(castle,(0,30))
 screen.blit(castle,(0,135))
 screen.blit(castle,(0,240))
 screen.blit(castle,(0,345 ))
-
-while (1):
+ 
+# 4 - keep looping through
+while 1:
+    # 5 - clear the screen before drawing it again
     screen.fill(0)
-    screen.blit(player,(100,100))
+    # 6 - draw the screen elements
+    screen.blit(player, (100,100))
+    # 7 - update the screen
     pygame.display.flip()
+    # 8 - loop through the events
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+        # check if the event is the X button 
+        if event.type==pygame.QUIT:
+            # if it is quit the game
+            pygame.quit() 
             exit(0)
